9c0d4b501ba0806de32526ff3c88bc2c54588e02
Andrius Alimas <alimasWebDeveloper@gmail.com>
2020-11-05 06:56:33 +0000

    In App component add two states todos and setTodos, then in Form component add function that handle button click event, setTodos, refresh state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
-const Form = ( {setInputText} ) => {
+const Form = ( {setInputText, inputText, todos, setTodos} ) => {
 
     // Here i can write js code and functions
     const inputTextHandler = (e) =>{
         console.log(e.target.value);
         setInputText(e.target.value);
     }
+
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            {
+                text: inputText, 
+                completed : false,
+                id: Math.random() * 1000
+            }            
+        ]);
+        setInputText(""); // reset state
+    }
+
     return(
         <form>
-        <input onChange={inputTextHandler} type="text" className="todo-input" />
-        <button className="todo-button" type="submit">
+        <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+        <button className="todo-button" type="submit" onClick = {submitTodoHandler}>
           <i className="fas fa-plus-square"></i>
         </button>
         <div className="select">
